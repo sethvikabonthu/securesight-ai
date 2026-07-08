60fd59c6eba8c5146495b12d073193759b2553a6
Sethvika Bonthu <sethvikabonthu@gmail.com>
2026-07-08 17:32:06 +0530

    Enable scans router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
+from app.routers import scans
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -14,6 +15,12 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+)
+
+app.include_router(
+    scans.router,
+    prefix="/scan",
+    tags=["Detection Scanners"]
 )
 
 @app.get("/")
